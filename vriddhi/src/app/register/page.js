71244a076f8a8c79fa5eef36e5cf4c983cb73b5d
Anirudha <72496314+4n1rudh4@users.noreply.github.com>
2023-11-13 19:54:17 +0530

    Update page.js

--- vriddhi/src/app/register/page.js
+++ vriddhi/src/app/register/page.js
@@ -469,7 +469,7 @@ An Excel sheet containing all the details of the students to their corresponding
             
             <div className='block m-auto text-red-500 text-center'>{error}</div>
             
-            <button disabled={submitdisable} onClick={handle} className=' flex border-2 text-center w-fit m-auto p-4  mt-8 rounded-xl border-[#0071C9] text-[#0071C9] hover:border-white hover:text-white' type="submit">Submit<div className={load} >
+            <button disabled={submitdisable} className=' flex border-2 text-center w-fit m-auto p-4  mt-8 rounded-xl border-[#0071C9] text-[#0071C9] hover:border-white hover:text-white' type="submit">Submit<div className={load} >
 <div role="status">
     <svg aria-hidden="true" class="w-6 h-6  text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
@@ -818,7 +818,7 @@ An Excel sheet containing all the details of the students to their corresponding
             /> </div>*/}
             <div className='block m-auto text-red-500 font-bold text-center'>{error}</div>
             
-            <button disabled={submitdisable} onClick={handle} className='text-white flex bg-[#0071C9] w-fit block m-auto p-2 rounded-xl hover:text-white' type="submit">Submit<div className={load} >
+            <button disabled={submitdisable} className='text-white flex bg-[#0071C9] w-fit block m-auto p-2 rounded-xl hover:text-white' type="submit">Submit<div className={load} >
 <div role="status">
     <svg aria-hidden="true" class="w-6 h-6  text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
